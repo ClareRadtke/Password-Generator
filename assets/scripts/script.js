5d714eb63ffa6466e56746c4cb0000cccbb806fc
Clare Radtke <clare.radtke@outlook.com>
2021-03-08 22:16:59 +1100

    Added in form with required criteria

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,8 +7,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Add an if statement - if >=1 radio button && length selected then generate password
