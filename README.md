8b926dc81a040d657a05be9509b71315ba0fa425
Clare Radtke <clare.radtke@outlook.com>
2021-03-14 18:06:11 +1100

    Resolved loop issue with cancel Prompt, values retrieved and validated from form. Form behaviour defined. GeneratePasssword function WIP

--- README.md
+++ README.md
@@ -2,25 +2,25 @@
 An application that generates random passwords based on criteria selected by the user.
 
 <!--- Deployed link ---> 
-![screenshot of my application](link.PNG?raw=true "passwordGeneratorScreenshot)
+<!-- ![screenshot of my application](link.PNG?raw=true "passwordGeneratorScreenshot) -->
 
 Requirements for this application:
 
 Functionality:
-  [] Generate Password button click presents a list of prompts for password criteria
-  [] Password criteria is selectable
+  [x] Generate Password button click presents a list of prompts for password criteria
+  [x] Password criteria is selectable
   [] Each prompt selection is validated
-  [] At least one character type should be selected
+  [x] At least one character type should be selected
   [] Password is generated that matches the selected criteria
   [] Generated password is displayed in an alert or written to the page
 
 Password criteria:
-  [] Password length can be between 8 and 128 characters inclusive
-  [] Character options for the password include:
-    [] Lowercase
-    [] Uppercase
-    [] Numeric
-    [] Special characters
+  [x] Password length can be between 8 and 128 characters inclusive
+  [x] Character options for the password include:
+    [x] Lowercase
+    [x] Uppercase
+    [x] Numeric
+    [x] Special characters
 
 Additional requirements:
   [] No errors produced in Chrome DevTools during use of application
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var data = {
+  length: null,
+  lowerCase: null,
+  upperCase: null,
+  numbers: null,
+  specialChar: null,
+};
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(data);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-
-//function to run the prompts for password criteria and popup form
+// Function to run the prompts for password criteria and popup form
 function openForm() {
   document.getElementById("criteriaForm").style.display = "block";
 }
@@ -28,7 +32,10 @@ function criteriaPrompts() {
   console.log(length, lengthValue);
 
   if (lengthValue >= 8 && lengthValue <= 128) {
+    data.length = lengthValue;
     openForm();
+  } else if (length == null) {
+    // Close prompt
   } else {
     alert(
       "Please enter the length of your password in numbers,\nit can be between 8 - 128 inclusive"
@@ -36,8 +43,97 @@ function criteriaPrompts() {
     criteriaPrompts();
   }
 }
-
+// Event listener to generate button to initiate Criteria prompt and form
 generateBtn.addEventListener("click", criteriaPrompts);
 
-// Add an if statement - if >=1 radio button && length selected then generate password
-//generateBtn.addEventListener("click", writePassword);
+// Preventing the form from default submission
+const criteriaForm = document.getElementById("form");
+criteriaForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  getCriteriaFormValues();
+  closeForm();
+  // TODO: Clear form
+  writePassword();
+});
+
+// Function to retrieve the radio button inputs on the Criteria form
+function getCriteriaFormValues() {
+  function getLowerCaseValue() {
+    var lowerCaseValue = document.getElementsByName("lowerCase");
+    for (var i = 0; i < lowerCaseValue.length; i++) {
+      if (lowerCaseValue[i].checked) {
+        console.log("Lower case values to be used " + lowerCaseValue[i].value);
+        data.lowerCase = lowerCaseValue[i].value === "true";
+      }
+    }
+  }
+  function getUpperCaseValue() {
+    var upperCaseValue = document.getElementsByName("upperCase");
+    for (var i = 0; i < upperCaseValue.length; i++) {
+      if (upperCaseValue[i].checked) {
+        console.log("Upper case values to be used " + upperCaseValue[i].value);
+        data.upperCase = upperCaseValue[i].value === "true";
+      }
+    }
+  }
+  function getNumbersValue() {
+    var numbersValue = document.getElementsByName("numbers");
+    for (var i = 0; i < numbersValue.length; i++) {
+      if (numbersValue[i].checked) {
+        console.log("Number values to be used " + numbersValue[i].value);
+        data.numbers = numbersValue[i].value === "true";
+      }
+    }
+  }
+  function getSpecialCharsValue() {
+    var specialCharsValue = document.getElementsByName("specialChar");
+    for (let i = 0; i < specialCharsValue.length; i++) {
+      if (specialCharsValue[i].checked) {
+        console.log(
+          "Special character values to be used " + specialCharsValue[i].value
+        );
+        data.specialChar = specialCharsValue[i].value === "true";
+      }
+    }
+  }
+  getLowerCaseValue();
+  getUpperCaseValue();
+  getNumbersValue();
+  getSpecialCharsValue();
+}
+
+function generatePassword(opts) {
+  console.log(opts);
+  const lowerCaseCharSet = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberCharSet = "0123456789";
+  const specialCharSet = "!@#$%^&*()\\/?<>'\":;[]{}`~|.,-_=+";
+  let superSet = "";
+  if (opts.lowerCase) {
+    superSet += lowerCaseCharSet;
+  }
+  if (opts.upperCase) {
+    superSet += upperCaseCharSet;
+  }
+  if (opts.numbers) {
+    superSet += numberCharSet;
+  }
+  if (opts.specialChar) {
+    superSet += specialCharSet;
+  }
+  console.log(superSet);
+  let output = "";
+  for (let i = 0; i < opts.length; i++) {
+    // TODO create output
+  }
+}
+
+// function generatePassword() {
+//   var length = 8,
+//     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//     retVal = "";
+//   for (var i = 0, n = charset.length; i < length; ++i) {
+//     retVal += charset.charAt(Math.floor(Math.random() * n));
+//   }
+//   return retVal;
+// }
